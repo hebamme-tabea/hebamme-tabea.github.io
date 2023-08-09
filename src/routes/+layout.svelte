a5203059df102534394298a9f02a1b22b28cf5a8
mattiskleine <hebamme-tabea@gmail.com>
2023-08-09 18:08:02 +0200

    landing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,81 @@
 <script>
 </script>
 
-<div class="header">test</div>
+<div class="header">
+	<a href="/">
+		<img src="logo.svg" alt="logo" class="logo" />
+	</a>
+	<div />
+	<a href="/" class="nav">home</a>
+	<a href="about" class="nav">about me</a>
+	<a href="contact" class="nav">contact</a>
+</div>
 
-<slot />
+<div>
+	<slot />
+</div>
 
 <style>
 	.header {
-		color: green;
+		display: grid;
+		height: 120px;
+		grid-template-columns: max-content auto repeat(3, max-content);
+		align-items: center;
+		gap: 60px;
+		padding: 0 60px 0 30px;
+	}
+
+	.logo {
+		height: 60px;
+	}
+
+	a {
+		text-decoration: none;
+		color: #444444;
+		font-size: 1.5rem;
+	}
+
+	.nav {
+		padding: 0.5rem 0.8rem;
+		position: relative;
+		font-size: 20px;
+	}
+
+	.nav::before,
+	.nav::after {
+		content: '';
+		height: 14px;
+		width: 14px;
+		position: absolute;
+		transition: all 0.35s ease;
+		opacity: 0;
+	}
+
+	.nav::before {
+		content: '';
+		right: 0;
+		top: 0;
+		border-top: 2px solid #666666;
+		border-right: 2px solid #aaaaaa;
+		transform: translate(-100%, 50%);
+	}
+
+	.nav:after {
+		content: '';
+		left: 0;
+		bottom: 0;
+		border-bottom: 2px solid #666666;
+		border-left: 2px solid #aaaaaa;
+		transform: translate(100%, -50%);
+	}
+
+	.nav:hover:before,
+	.nav:hover:after {
+		transform: translate(0, 0);
+		opacity: 1;
+	}
+
+	.nav:hover {
+		color: black;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,104 @@
 <script>
+	//Das Mutter-Sein ist etwas völlig Neues.
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<div class="main">test main</div>
+<div class="grey-box" />
+<div class="grid">
+	<img src="profilbild.jpeg" alt="profile" class="profile-img" />
+	<div class="text-section">
+		<div class="name">TABEA KLEINE</div>
+		<div class="profession">Hebamme</div>
+		<img src="logo.svg" alt="logo" class="logo" />
+		<div class="quote">
+			<img src="quote-bg.png" alt="quote" class="quote-background" />
+			<div class="quote-body">
+				In dem Moment, in dem ein Kind geboren wird, wird auch eine Mutter geboren. Die Frau hat
+				vorher schon existiert, jedoch nicht als Mutter. Die Mutter ist genauso neu wie das Kind.
+			</div>
+			<div class="quote-author">- Bhagwan Shree Rajneesh</div>
+		</div>
+		<div class="skills">Schwangerenvorsorge &#183; Wochenbettbetreuung</div>
+	</div>
+</div>
 
 <style>
-	.main {
-		background-color: grey;
+	.grey-box {
+		position: absolute;
+		background-color: #eeeeee;
+		left: 0;
+		top: 0;
+		width: calc(300px + 10%);
+		height: 840px;
+		min-height: 100%;
+		z-index: -1;
+		background-image: url(background.png);
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+
+	.grid {
+		display: grid;
+		grid-template-columns: 400px 1fr;
+		margin: 0 10%;
+		gap: 50px;
+	}
+
+	.profile-img {
+		width: 100%;
+	}
+
+	.text-section {
+		width: 100%;
+		padding-top: 40px;
+		color: #444444;
+	}
+
+	.name {
+		font-size: 2rem;
+		letter-spacing: 0.3rem;
+	}
+
+	.profession {
+		font-family: Brittany Signature;
+		font-size: 2.3rem;
+		padding-top: 20px;
+	}
+
+	.logo {
+		width: 400px;
+		margin-top: 50px;
+		margin-left: 200px;
+		display: none;
+	}
+
+	.quote {
+		margin: 20px 0;
+		position: relative;
+		width: 800px;
+	}
+
+	.quote-background {
+		width: 100%;
+	}
+
+	.quote-body {
+		position: absolute;
+		font-size: 1.3rem;
+		top: 28%;
+		text-align: center;
+		width: 47%;
+		left: 28%;
+	}
+
+	.quote-author {
+		position: absolute;
+		bottom: 20%;
+		right: 25%;
+	}
+
+	.skills {
+		font-style: italic;
+		text-align: center;
+		font-size: 1.1rem;
 	}
 </style>

dcbcc0e80450cca6731ae8c5f84b79f4df155c12
mattiskleine <hebamme-tabea@gmail.com>
2023-08-09 22:54:34 +0200

    Updates

---
+++ _app/immutable/chunks/paths.f5be4b9e.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_40t59a)==null?void 0:s.base)??"/hebamme-tabea.github.io";var a;const e=((a=globalThis.__sveltekit_40t59a)==null?void 0:a.assets)??t;export{e as a,t as b};

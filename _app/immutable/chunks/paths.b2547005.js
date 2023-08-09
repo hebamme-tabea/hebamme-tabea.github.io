a878bc0bd8490804037a41e3281e663f021eca88
mattiskleine <hebamme-tabea@gmail.com>
2023-08-09 21:58:48 +0200

    update

---
+++ _app/immutable/chunks/paths.b2547005.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_pa28tv)==null?void 0:s.base)??"/sveltekit-github-pages";var t;const e=((t=globalThis.__sveltekit_pa28tv)==null?void 0:t.assets)??a;export{e as a,a as b};

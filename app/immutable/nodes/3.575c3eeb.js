060d3fc50c02087e2c3fe93d6affc3c389f8d194
mattiskleine <hebamme-tabea@gmail.com>
2023-12-07 08:13:49 +0100

    Updates

---
+++ app/immutable/nodes/3.575c3eeb.js
@@ -0,0 +1,11 @@
+import{s as b,n as a}from"../chunks/scheduler.cd4e5936.js";import{S as f,i as v,g as m,s as p,h as o,x as h,c as w,k as u,a as r,f as l}from"../chunks/index.b35f5289.js";const _=!0,y=Object.freeze(Object.defineProperty({__proto__:null,prerender:_},Symbol.toStringTag,{value:"Module"}));function j(g){let e,d='<img src="images/background.png" alt="background" class="bg-img svelte-1sfmwjj"/>',s,t,c=`<img src="images/babyfeet.jpg" alt="babyfoot" class="babyfeet-img svelte-1sfmwjj"/> <div class="title svelte-1sfmwjj"><h1 class="name svelte-1sfmwjj">TABEA KLEINE</h1> <h2 class="profession svelte-1sfmwjj">Hebamme</h2></div> <div class="text svelte-1sfmwjj">Ich bin Tabea und 1993 in Heilbronn, Baden Württemberg geboren. Aufgewachsen bin ich in der
+		Gegend von Heidelberg und 2015 für die Hebammenausbildung nach Hamburg gezogen. Dabei hat mich
+		besonders die Veränderung des weiblichen Körpers während einer Schwangerschaft fasziniert und
+		die enorme Leistung die Frauen in der Schwangerschaft, unter der Geburt und im Wochenbett
+		erbringen. Nach der Ausbildung bin ich selbst schwanger geworden und litt dabei die kompletten 9
+		Monate an extremer Schwangerschaftsübelkeit. Dadurch ist mein Respekt vor Frauen in der
+		Schwangerschaft noch einmal gestiegen. In meiner Arbeit möchte ich daher meine Aufmerksamkeit
+		nicht nur auf das Wohlergehen des Neugeborenen, sondern auch besonders auf das Wohlergehen der
+		Mutter richten, denn eine Entbindung gehört zu den bedeutendsten Initationen im Leben einer
+		Frau. Denn wenn ein Kind geboren wird, kommt auch eine Mutter zur Welt. Nach einem Jahr
+		Elternzeit meines zweiten Kindes, steige ich wieder in meinen Beruf ein.</div>`;return{c(){e=m("div"),e.innerHTML=d,s=p(),t=m("div"),t.innerHTML=c,this.h()},l(n){e=o(n,"DIV",{class:!0,"data-svelte-h":!0}),h(e)!=="svelte-175mtm8"&&(e.innerHTML=d),s=w(n),t=o(n,"DIV",{class:!0,"data-svelte-h":!0}),h(t)!=="svelte-1wivdq1"&&(t.innerHTML=c),this.h()},h(){u(e,"class","grey-box svelte-1sfmwjj"),u(t,"class","grid svelte-1sfmwjj")},m(n,i){r(n,e,i),r(n,s,i),r(n,t,i)},p:a,i:a,o:a,d(n){n&&(l(e),l(s),l(t))}}}class H extends f{constructor(e){super(),v(this,e,null,j,b,{})}}export{H as component,y as universal};

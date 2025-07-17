6197b35f4075d6d934e224cbf6f092991368a692
hebamme-tabea <hebamme-tabea@gmail.com>
2025-07-17 11:55:11 +0200

    Updates

---
+++ app/immutable/chunks/profilbild.5fb7db64.js
@@ -0,0 +1,1 @@
+const e=""+new URL("../assets/profilbild.2b191a04.jpg",import.meta.url).href;export{e as p};
---
+++ app/immutable/nodes/3.bf5d0fd5.js
@@ -0,0 +1,10 @@
+import{s,n as t}from"../chunks/scheduler.cd4e5936.js";import{S as l,i as d,g as o,h as c,x as h,k as u,a as g,f as m}from"../chunks/index.b35f5289.js";import{p as b}from"../chunks/profilbild.5fb7db64.js";const f=!0,w=Object.freeze(Object.defineProperty({__proto__:null,prerender:f},Symbol.toStringTag,{value:"Module"}));function v(a){let e,r=`<img src="${b}" alt="babyfoot" class="babyfeet-img svelte-1j1l3x8"/> <div class="title svelte-1j1l3x8"><h1 class="name svelte-1j1l3x8">TABEA KLEINE</h1> <h2 class="profession svelte-1j1l3x8">Hebamme</h2></div> <div class="text svelte-1j1l3x8">Ich bin Tabea und 1993 in Heilbronn, Baden Württemberg geboren. Aufgewachsen bin ich in der
+		Gegend von Heidelberg und 2015 für die Hebammenausbildung nach Hamburg gezogen. Dabei hat mich
+		besonders die Veränderung des weiblichen Körpers während einer Schwangerschaft fasziniert und
+		die enorme Leistung die Frauen in der Schwangerschaft, unter der Geburt und im Wochenbett
+		erbringen. Nach der Ausbildung bin ich selbst schwanger geworden und litt dabei die kompletten 9
+		Monate an extremer Schwangerschaftsübelkeit. Dadurch ist mein Respekt vor Frauen in der
+		Schwangerschaft noch einmal gestiegen. In meiner Arbeit möchte ich daher meine Aufmerksamkeit
+		nicht nur auf das Wohlergehen des Neugeborenen, sondern auch besonders auf das Wohlergehen der
+		Mutter richten, denn eine Entbindung gehört zu den bedeutendsten Initationen im Leben einer
+		Frau. Denn wenn ein Kind geboren wird, kommt auch eine Mutter zur Welt.</div>`;return{c(){e=o("div"),e.innerHTML=r,this.h()},l(n){e=c(n,"DIV",{class:!0,"data-svelte-h":!0}),h(e)!=="svelte-5abkga"&&(e.innerHTML=r),this.h()},h(){u(e,"class","grid svelte-1j1l3x8")},m(n,i){g(n,e,i)},p:t,i:t,o:t,d(n){n&&m(e)}}}class j extends l{constructor(e){super(),d(this,e,null,v,s,{})}}export{j as component,w as universal};
